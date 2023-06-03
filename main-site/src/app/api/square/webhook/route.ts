d635152f19170079b49cfe22c013f78f06e30cdd
Sam Larsen-Disney <sam@amplitude.com>
2023-06-03 06:23:55 -0700

    adding webhook route

--- main-site/src/app/api/square/webhook/route.ts
+++ main-site/src/app/api/square/webhook/route.ts
@@ -1,6 +1,13 @@
 
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(req: NextRequest) {
+  console.log(req)
+  return NextResponse.json({
+    message: "Hello, World!",
+    });
+}
+
 
 export async function POST(req: NextRequest) {
   console.log(req.body)
