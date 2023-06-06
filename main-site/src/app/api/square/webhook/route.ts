41a2a38fbb96a73735246256001b993143efae6d
Sam Larsen-Disney <sam@amplitude.com>
2023-06-05 23:33:46 -0700

    webhook fix

--- main-site/src/app/api/square/webhook/route.ts
+++ main-site/src/app/api/square/webhook/route.ts
@@ -1,14 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  console.log(req);
-  return NextResponse.json({
-    status: 200,
-  });
-}
+import kv from "@vercel/kv";
 
 export async function POST(req: NextRequest) {
-  console.log(req.body);
+
+  const body = await req.json();
+
+  // square webhook handle purchase complete
+
+    switch (body?.data?.type) {
+      case "payment.created":
+        if(body?.data?.status === "COMPLETED") {
+          const [siteId, userId] = body?.data?.payment_note.split("|");
+          const user = await kv.hgetall(userId as string) as {
+            subscripedSites?: string[];
+            [key: string]: any;
+          };
+        
+          if (!user.subscripedSites) {
+            user.subscripedSites = [];
+          }
+          if(!user.subscripedSites.includes(siteId)) {
+        
+          user.subscripedSites.push(siteId);
+        
+          }
+
+          await kv.hset(userId as string, user);
+          
+        }
+        break;
+    }
+
   return NextResponse.json({
     status: 200,
   });
